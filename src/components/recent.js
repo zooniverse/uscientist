10e098b718cbd82e77783bbcfaea941bba3b0fba
Will Granger <twgranger@gmail.com>
2019-04-24 15:55:37 -0500

    Finish Recent Section

---
+++ src/components/recent.js
@@ -0,0 +1,27 @@
+import React from "react"
+import recentStyles from "./recent.module.css"
+
+const placeholder = ["SBJ.26440667", "SBJ.26440667", "SBJ.26440667"];
+
+const Recent = () => (
+    <div className={recentStyles.recent}>
+      <h3>Recent galaxies</h3>
+      <span className={recentStyles.content}>
+        People just like you have been classifying galaxies from U!Scientist at
+        the Adler as well as online on Galaxy Zoo.
+      </span>
+      <div className={recentStyles.subjects}>
+        {placeholder.map(image =>
+            <div className={recentStyles.subject}>
+              <div />
+              <span>{image}</span>
+            </div>
+        )}
+      </div>
+      <button className='hollow-button'>
+        <a href='https://www.galaxyzoo.org'>see more at galaxyzoo.org</a>
+      </button>
+    </div>
+);
+
+export default Recent;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import React from "react"
 import Layout from "../components/layout"
 import CallToAction from "../components/CallToAction"
 import Divider from "../components/divider"
-import General from "../components/General"
+import General from "../components/general"
+import Recent from "../components/recent"
 
 const IndexPage = () => (
   <Layout>
-    <div style={{ background: `#001133`, padding: `10rem` }}>
-      <CallToAction/>
+    <div style={{ background: `#001133`, padding: `15%` }}>
+      <CallToAction />
       <Divider />
+      <Recent />
       <Divider />
       <Divider />
       <General />
