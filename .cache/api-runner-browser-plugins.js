0cc72c67297c4bae6c1946bd92432de59588ad26
Will Granger <twgranger@gmail.com>
2019-04-25 15:36:34 -0500

    Remove Public and Cache Files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/wgranger/Zooniverse/touch-table-intermediate/node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
-      options: {"plugins":[],"google":{"families":["Poppins","IBM Plex Mono"]}},
-    },{
-      plugin: require('/Users/wgranger/Zooniverse/touch-table-intermediate/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
