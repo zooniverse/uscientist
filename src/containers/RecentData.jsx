320d2ee440e1c72ac373c9fc2cc439816c5eb94c
Will Granger <twgranger@gmail.com>
2019-05-10 10:19:42 -0500

    Attempt to Use GraphQL-Request

--- src/containers/RecentData.jsx
+++ src/containers/RecentData.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types"
-import Data from "../components/data";
-import statsClient from "panoptes-client/lib/stats-client";
-import { config } from "../config";
+import { GraphQLClient } from 'graphql-request'
+import Data from "../components/data"
+// import { config } from "../config"
+
+export const statsClient = new GraphQLClient('https://graphql-stats.zooniverse.org/graphql', {
+  credentials: 'include',
+  mode: 'cors'
+});
 
 class RecentData extends React.Component {
   constructor(props) {
@@ -15,39 +20,58 @@ class RecentData extends React.Component {
   }
 
   componentDidMount() {
-    this.setTotalCount();
-    this.setTableCount();
+    // this.setTotalCount();
+    // this.setTableCount();
+    this.fetchGraphQLQuery();
   }
 
-  setTotalCount() {
-    statsClient
-      .query({
-        projectID: config.projectID,
-        period: "day",
-        type: "classification"
-      })
-      .then((data) => {
-        const mostRecentCount = data[data.length - 1];
-        if (mostRecentCount && mostRecentCount.doc_count) {
-          this.setState({ totalDaily: mostRecentCount.doc_count });
-        }
+  fetchGraphQLQuery() {
+    const query = `{
+      statsCount(
+        eventType: "classification",
+        interval: "1 Day",
+        projectId: "5733"
+      ){
+        period,
+        count
+      }
+    }`
+
+    statsClient.request(query.replace(/\s+/g, ' '))
+      .then((result) => {
+        console.log(result);
       })
   }
 
-  setTableCount() {
-    statsClient
-      .query({
-        workflowID: config.tableWorkflowID,
-        period: "day",
-        type: "classification"
-      })
-      .then((data) => {
-        const mostRecentCount = data[data.length - 1];
-        if (mostRecentCount && mostRecentCount.doc_count) {
-          this.setState({ tableCount: mostRecentCount.doc_count });
-        }
-      })
-  }
+  // setTotalCount() {
+  //   statsClient
+  //     .query({
+  //       projectID: config.projectID,
+  //       period: "day",
+  //       type: "classification"
+  //     })
+  //     .then((data) => {
+  //       const mostRecentCount = data[data.length - 1];
+  //       if (mostRecentCount && mostRecentCount.doc_count) {
+  //         this.setState({ totalDaily: mostRecentCount.doc_count });
+  //       }
+  //     })
+  // }
+  //
+  // setTableCount() {
+  //   statsClient
+  //     .query({
+  //       workflowID: config.tableWorkflowID,
+  //       period: "day",
+  //       type: "classification"
+  //     })
+  //     .then((data) => {
+  //       const mostRecentCount = data[data.length - 1];
+  //       if (mostRecentCount && mostRecentCount.doc_count) {
+  //         this.setState({ tableCount: mostRecentCount.doc_count });
+  //       }
+  //     })
+  // }
 
   render() {
     return (
